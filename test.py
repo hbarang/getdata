3a3cec482bbda1de6f2c6bb1066a1b8628848642
hbarang <hbarangenc@gmail.com>
2018-02-28 18:57:27 +0300

    simple improvements

--- test.py
+++ test.py
@@ -1,18 +1,24 @@
 from selenium import webdriver
+from selenium.common.exceptions import InvalidArgumentException
 
+# relative path to gecko driver
 browser = webdriver.Firefox(executable_path="geckodriver.exe")
 
 
+# getting input
 def get_url():
-    url = input("Enter url")
-    return url
+    url = input("Enter the url: ")
+    return str(url)
 
 
+# getting price defined as offering price
 def get_price():
     browser.get(get_url())
     test = browser.find_element_by_id("offering-price")
     cont = test.text
     print(cont)
+
+
 
 
 def main():
